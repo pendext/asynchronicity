d43954e6127a03a50e81c51b92280c74bc8269a4
John Pendexter <jpendexter@manifestcorp.com>
2015-10-10 13:22:54 -0400

    Adding joda time to log so it can clearly be seen that the long running process is taking 10 seconds.

--- pendext-async/src/main/java/com/pendext/asynchronocity/app/services/ApplicationNotRespondingInvokerService.java
+++ pendext-async/src/main/java/com/pendext/asynchronocity/app/services/ApplicationNotRespondingInvokerService.java
@@ -1,14 +1,15 @@
 package com.pendext.asynchronocity.app.services;
 
 import android.util.Log;
+import org.joda.time.DateTime;
 
 public class ApplicationNotRespondingInvokerService {
     private static final String TAG = "ANRInvokerService";
     public void invokeApplicationNotResponding() {
         try {
-            Log.i(TAG, "Invoking long running process at " + System.currentTimeMillis());
+            Log.i(TAG, "Invoking long running process at " + new DateTime().toString());
             Thread.sleep(10000);
-            Log.i(TAG, "Completing long running process at " + System.currentTimeMillis());
+            Log.i(TAG, "Completing long running process at " + new DateTime().toString());
         } catch (InterruptedException e) {}
     }
 }
