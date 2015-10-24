8d9bb6da618c6f5571a6dc605433c0bdc8ffa9bd
John Pendexter <john.pendexter@gmail.com>
2015-10-24 13:38:47 -0400

    Basic async task functionality is working.

--- pendext-async/src/main/java/com/pendext/asynchronocity/app/asynctask/BasicAsyncTask.java
+++ pendext-async/src/main/java/com/pendext/asynchronocity/app/asynctask/BasicAsyncTask.java
@@ -2,10 +2,13 @@ package com.pendext.asynchronocity.app.asynctask;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
+import org.joda.time.LocalTime;
 
 public class BasicAsyncTask extends AsyncTask<String, Integer, String> {
 
     private TextView textView;
+    private String startTime;
+    private String endTime;
 
     public BasicAsyncTask(TextView textView) {
         this.textView = textView;
@@ -13,6 +16,12 @@ public class BasicAsyncTask extends AsyncTask<String, Integer, String> {
 
     @Override
     protected String doInBackground(String... params) {
+        try {
+            this.startTime = params[0];
+            Thread.sleep(20000);
+            this.endTime = new LocalTime().toString("hh:mm:ss");
+
+        } catch (InterruptedException e) {}
         return null;
     }
 
@@ -22,6 +31,9 @@ public class BasicAsyncTask extends AsyncTask<String, Integer, String> {
 
     @Override
     protected void onPostExecute(String result) {
+        String text = "The async task began a long running task at " + startTime +
+                            " and completed the task at " + endTime;
+        textView.setText(text);
     }
 
 }
