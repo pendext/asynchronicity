0b0a0b4602728d29022002b9469fe3bf779f83fb
John Pendexter <john.pendexter@gmail.com>
2015-11-09 18:50:28 -0500

    RxJava example is all ready to go! Boom!

--- pendext-async/src/main/java/com/pendext/asynchronocity/app/fragments/ReactiveFragment.java
+++ pendext-async/src/main/java/com/pendext/asynchronocity/app/fragments/ReactiveFragment.java
@@ -3,9 +3,9 @@ package com.pendext.asynchronocity.app.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.*;
-import android.widget.Button;
+import android.widget.*;
 import com.pendext.asynchronocity.app.R;
-import com.pendext.asynchronocity.app.listeners.ButtonForShowingUIThreadIsActiveOnClickListener;
+import com.pendext.asynchronocity.app.listeners.*;
 
 public class ReactiveFragment extends Fragment {
 
@@ -14,10 +14,11 @@ public class ReactiveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.reactive_example, container, false);
-
+        TextView textViewToUpdate = (TextView) rootView.findViewById(R.id.reactive_text1);
         Button uiWorkButton = (Button) rootView.findViewById(R.id.reactive_ui_work);
+        Button exampleButton = (Button) rootView.findViewById(R.id.reactive_invoke_button);
+        exampleButton.setOnClickListener(new ReactiveExampleOnClickListener(textViewToUpdate));
         uiWorkButton.setOnClickListener(new ButtonForShowingUIThreadIsActiveOnClickListener(getResources().getString(R.string.unblocked_toast_test)));
-
         return rootView;
     }
 }
